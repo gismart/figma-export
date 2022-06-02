49522e83a52414fee96c8afc72857ac368d46272
Ivan Sokolov <ivan.sokolov@gismart.com>
2022-06-02 15:57:05 +0200

    Up Version

--- Sources/FigmaExport/main.swift
+++ Sources/FigmaExport/main.swift
@@ -31,7 +31,7 @@ enum FigmaExportError: LocalizedError {
 
 struct FigmaExportCommand: ParsableCommand {
     
-    static let version = "0.37.0"
+    static let version = "0.38.0"
     
     static let svgFileConverter = VectorDrawableConverter()
     static let fileWriter = FileWriter()
